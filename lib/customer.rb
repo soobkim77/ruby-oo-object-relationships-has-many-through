ceedb4dc55ea2ffe72d2d7184fb449bc0ea585bb
Soo-Beom <soobeomkim77@gmail.com>
2021-04-08 13:32:57 -0700

    commit tree 0f3fb04ebff9142822362410bb4af62161e3b0c9

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,29 @@
 class Customer
-  
+    attr_reader :name, :age
+
+    @@all = []
+
+    def initialize(name, age)
+      @name = name
+      @age = age
+      @@all << self
+    end
+
+    def self.all 
+      @@all
+    end
+
+    def new_meal(waiter, total, tip)
+      meal = Meal.new(waiter, self, total, tip)
+      meal
+    end
+
+    def meals
+      Meal.all.select{|meal| meal.customer == self}
+    end
+
+    def waiters 
+      meals.map{|meal| meal.waiter} 
+    end
+
 end
--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,32 @@
 class Waiter
+    attr_reader :name, :yrs_experience
+
+    @@all = [] 
+
+    def initialize(name, yrs_experience)
+        @name = name
+        @yrs_experience = yrs_experience
+        @@all << self
+    end
+    
+    def self.all
+        @@all
+    end
+
+    def new_meal(customer, total, tip)
+        meal = Meal.new(self, customer, total, tip)
+        meal
+    end
+
+    def meals
+        Meal.all.select{|meal| meal.waiter == self}
+    end
+
+    def best_tipper
+        best_tip = meals.max do |meal_1, meal_2|
+            meal_1.tip <=> meal_2.tip
+        end
+        best_tip.customer
+    end
 
 end
